c806b23335177ed4575e173578bd50111d8f9cff
amably <alex.mably@gmail.com>
2019-11-21 10:16:39 -0600

    update numpy package and bump version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='ShapExplainers',
-    version='1.0.0',
+    version='1.0.1',
     description='Wrappers for shapley value explanations for model interpretability',
     packages=['ShapExplainers'],
     install_requires=["typing",
                       "pandas",
-                      "numpy == 1.15.4",
+                      "numpy == 1.17.3",
                       'scikit-learn',
                       "shap == 0.29.3",
                       ],
